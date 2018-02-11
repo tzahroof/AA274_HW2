60dac5e06858d10f4224a55ca926e782593e2475
Ed Schmerling <ednerd@gmail.com>
2018-02-11 15:50:54 -0800

    Problem 1 and 2 submission instructions

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-Problems 5 and submission instructions forthcoming.
+### Submission Instructions
+
+#### Writeup
+All solution writeups (math, plots, explanations) in this class must be submitted electronically as a .pdf file. Preferably this writeup will be produced using LaTeX or Word, but if your submission will be handwritten and scanned, please use a flatbed scanner or a scanning app on your phone (raw photos will not be accepted). Name your writeup `<your-sunet-id>.pdf` (e.g., `pavone.pdf`) and place it in the base level of this directory (i.e., at `.../AA274_HW2/pavone.pdf`). For now, this pdf may contain just your writeup for problems 1 and 2; you may extend this pdf when submitting problems 3, 4, and 5.
+
+#### Submission
+To download the submission script, open this directory (i.e., `.../AA274_HW2/`) in your terminal and run `git pull` at the command line. Then run `./submit_hw2_p12.sh` and follow the instructions to submit!
---
+++ submit_hw2_p12.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+read -p "Please enter your SUNetID: " sunetid
+# read -p "Enter the location of your catkin_workspace (to default to $HOME/catkin_ws/, press Enter): " catkin_ws
+# catkin_ws=${catkin_ws:-$HOME/catkin_ws/}
+
+rm -f "${sunetid}_hw2_p12.zip"
+echo "Creating ${sunetid}_hw2_p12.zip"
+zip -q "${sunetid}_hw2_p12.zip" "submit_hw2_p12.sh"
+zip -qd "${sunetid}_hw2_p12.zip" "submit_hw2_p12.sh" # making an empty zip file
+
+for fname in "Problem_1/cal_workspace.py" \
+             "Problem_1/cam_calibrator.py" \
+             "Problem_2/ExtractLines.py"
+do
+    if [ -f $fname ]; then
+        zip "${sunetid}_hw2_p12.zip" $fname
+    else
+        read -p "$fname not found. Skip it? [yn]: " yn
+        case $yn in
+            [Yy]* ) ;;
+            * ) exit;;
+        esac
+    fi
+done
+
+if [ -f "$sunetid.pdf" ]; then
+    zip "${sunetid}_hw2_p12.zip" "$sunetid.pdf"
+else
+    echo "Cannot find ./$sunetid.pdf; you must submit your HW2 writeup as $sunetid.pdf in this directory."
+    exit
+fi
+
+echo ""
+echo "Querying Stanford server (AFS) to determine submission number; enter SUNetID password if prompted."
+ssh_result=$(ssh -o NumberOfPasswordPrompts=1 -o ControlMaster=auto -o ControlPersist=yes -o ControlPath=~/.ssh/%r@%h:%p $sunetid@cardinal.stanford.edu ls -t /afs/ir/class/aa274/HW2 2>/dev/null)
+ssh_success=$?
+lastsub=$(echo ${ssh_result} | tr ' ' '\n' | grep -m 1 ${sunetid}_hw2_p12_submission_[0-9]*.zip | grep -Eo 'submission_[0-9]{1,4}' | grep -Eo '[0-9]{1,4}') # very unorthodox
+if [ $ssh_success -eq "255" ]; then
+    tput setaf 1
+    tput bold
+    echo "
+             / ██████╗ ███████╗████████╗██████╗ ██╗   ██╗██╗
+  \    /\   |  ██╔══██╗██╔════╝╚══██╔══╝██╔══██╗╚██╗ ██╔╝██║
+   )  ( ') <   ██████╔╝█████╗     ██║   ██████╔╝ ╚████╔╝ ██║
+  (  /  )   |  ██╔══██╗██╔══╝     ██║   ██╔══██╗  ╚██╔╝  ╚═╝
+   \(__)|   |  ██║  ██║███████╗   ██║   ██║  ██║   ██║   ██╗
+             \ ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝
+An error occurred when connecting to the submission server. Please retry.
+(One cause of this error message is entering your password incorrectly.)"
+    exit
+fi
+
+subnum=$((lastsub + 1))
+echo "Copying to AFS (running command below); enter SUNetID password if prompted."
+set -x
+scp -o NumberOfPasswordPrompts=1 -o ControlMaster=auto -o ControlPersist=yes -o ControlPath=~/.ssh/%r@%h:%p "${sunetid}_hw2_p12.zip" "$sunetid@cardinal.stanford.edu:/afs/ir/class/aa274/HW2/${sunetid}_hw2_p12_submission_$subnum.zip" 2>/dev/null
+ssh_success=$?
+{ set +x; } 2>/dev/null
+
+if [ $ssh_success -eq "255" ]; then
+    tput setaf 1
+    tput bold
+    echo "
+             / ██████╗ ███████╗████████╗██████╗ ██╗   ██╗██╗
+  \    /\   |  ██╔══██╗██╔════╝╚══██╔══╝██╔══██╗╚██╗ ██╔╝██║
+   )  ( ') <   ██████╔╝█████╗     ██║   ██████╔╝ ╚████╔╝ ██║
+  (  /  )   |  ██╔══██╗██╔══╝     ██║   ██╔══██╗  ╚██╔╝  ╚═╝
+   \(__)|   |  ██║  ██║███████╗   ██║   ██║  ██║   ██║   ██╗
+             \ ╚═╝  ╚═╝╚══════╝   ╚═╝   ╚═╝  ╚═╝   ╚═╝   ╚═╝
+An error occurred when connecting to the submission server. Please retry.
+(One cause of this error message is entering your password incorrectly.)"
+    exit
+else
+    tput setaf 2
+    tput bold
+    echo "
+             / ███████╗██╗   ██╗ ██████╗ ██████╗███████╗███████╗███████╗██╗
+  \    /\   |  ██╔════╝██║   ██║██╔════╝██╔════╝██╔════╝██╔════╝██╔════╝██║
+   )  ( ') <   ███████╗██║   ██║██║     ██║     █████╗  ███████╗███████╗██║
+  (  /  )   |  ╚════██║██║   ██║██║     ██║     ██╔══╝  ╚════██║╚════██║╚═╝
+   \(__)|   |  ███████║╚██████╔╝╚██████╗╚██████╗███████╗███████║███████║██╗
+             \ ╚══════╝ ╚═════╝  ╚═════╝ ╚═════╝╚══════╝╚══════╝╚══════╝╚═╝"
+fi
